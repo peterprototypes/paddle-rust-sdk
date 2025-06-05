9f74d5e64f0d9249a604a6250b4d9bb2dd65410d
Denis Kolodin <deniskolodin@gmail.com>
2025-06-05 05:51:51 +0200

    Derive Eq, Ord and Hash traits for ids

--- src/ids.rs
+++ src/ids.rs
@@ -5,7 +5,7 @@ use std::fmt::Display;
 use serde::{Deserialize, Serialize};
 
 /// Unique Paddle ID for this address entity, prefixed with `add_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct AddressID(String);
 
 impl<T: Display> From<T> for AddressID {
@@ -21,7 +21,7 @@ impl AsRef<str> for AddressID {
 }
 
 /// Unique Paddle ID for this customer entity, prefixed with `ctm_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct CustomerID(String);
 
 impl<T: Display> From<T> for CustomerID {
@@ -37,7 +37,7 @@ impl AsRef<str> for CustomerID {
 }
 
 /// Unique Paddle ID for this adjustment entity, prefixed with `adj_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct AdjustmentID(String);
 
 impl<T: Display> From<T> for AdjustmentID {
@@ -53,7 +53,7 @@ impl AsRef<str> for AdjustmentID {
 }
 
 /// Unique Paddle ID for this transaction entity, prefixed with `txn_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct TransactionID(String);
 
 impl<T: Display> From<T> for TransactionID {
@@ -68,7 +68,7 @@ impl AsRef<str> for TransactionID {
     }
 }
 /// Unique Paddle ID for this subscription entity, prefixed with `sub_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SubscriptionID(String);
 
 impl<T: Display> From<T> for SubscriptionID {
@@ -84,15 +84,15 @@ impl AsRef<str> for SubscriptionID {
 }
 
 /// Unique Paddle ID for this transaction item, prefixed with `txnitm_`. Used when working with [adjustments](https://developer.paddle.com/build/transactions/create-transaction-adjustments).
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct TransactionItemID(String);
 
 /// Unique Paddle ID for this adjustment item, prefixed with `adjitm_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct AdjustmentItemID(String);
 
 /// Unique Paddle ID for this business entity, prefixed with `biz_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct BusinessID(String);
 
 impl<T: Display> From<T> for BusinessID {
@@ -108,7 +108,7 @@ impl AsRef<str> for BusinessID {
 }
 
 /// Unique Paddle ID for this payment method entity, prefixed with `paymtd_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct PaymentMethodID(String);
 
 impl<T: Display> From<T> for PaymentMethodID {
@@ -124,11 +124,11 @@ impl AsRef<str> for PaymentMethodID {
 }
 
 /// Unique Paddle ID for this customer portal session entity, prefixed with `cpls_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct CustomerPortalSessionID(String);
 
 /// Unique Paddle ID for this discount, prefixed with `dsc_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct DiscountID(String);
 
 impl<T: Display> From<T> for DiscountID {
@@ -144,11 +144,11 @@ impl AsRef<str> for DiscountID {
 }
 
 /// Unique code that customers can use to apply this discount at checkout. Use letters and numbers only, up to 16 characters. Not case-sensitive.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct DiscountCode(String);
 
 /// Unique Paddle ID for this event, prefixed with `evt_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct EventID(String);
 
 impl<T: Display> From<T> for EventID {
@@ -164,7 +164,7 @@ impl AsRef<str> for EventID {
 }
 
 /// Unique Paddle ID for this price, prefixed with `pri_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct PriceID(String);
 
 impl<T: Display> From<T> for PriceID {
@@ -180,7 +180,7 @@ impl AsRef<str> for PriceID {
 }
 
 /// Unique Paddle ID for this product, prefixed with `pro_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct ProductID(pub String);
 
 impl<T: Display> From<T> for ProductID {
@@ -197,7 +197,7 @@ impl AsRef<str> for ProductID {
 }
 
 /// Unique Paddle ID for API keys, prefixed with `apikey_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct ApiKeyID(pub String);
 
 impl<T: Display> From<T> for ApiKeyID {
@@ -213,7 +213,7 @@ impl AsRef<str> for ApiKeyID {
 }
 
 /// Unique Paddle ID for payouts, prefixed with `payout_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct PayoutID(pub String);
 
 impl<T: Display> From<T> for PayoutID {
@@ -229,23 +229,23 @@ impl AsRef<str> for PayoutID {
 }
 
 /// Unique Paddle ID for this notification, prefixed with `ntf_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct NotificationID(String);
 
 /// Unique Paddle ID for this notification setting, prefixed with `ntfset_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct NotificationSettingID(String);
 
 /// Unique Paddle ID for this notification log, prefixed with `ntflog_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct NotificationLogID(String);
 
 /// Webhook destination secret key, prefixed with `pdl_ntfset_`. Used for signature verification.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct EndpointSecretKey(String);
 
 /// Just a Paddle ID. I've noticed this used in some places.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct PaddleID(String);
 
 impl<T: Display> From<T> for PaddleID {
@@ -262,17 +262,17 @@ impl AsRef<str> for PaddleID {
 }
 
 /// Unique Paddle ID for this simulation event, prefixed with `ntfsimevt_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SimulationEventID(String);
 
 /// Unique Paddle ID for this simulation run, prefixed with `ntfsimrun_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SimulationRunID(String);
 
 /// Unique Paddle ID for this simulation, prefixed with `ntfsim_`.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SimulationID(String);
 
 /// Paddle ID of the invoice that this transaction is related to, prefixed with `inv_`. Used for compatibility with the Paddle Invoice API, which is now deprecated. This field is scheduled to be removed in the next version of the Paddle API.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct InvoiceId(String);
